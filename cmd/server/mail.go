93fd8db0ac6b177d5830c4f83430331283d5b15a
Henry Osei <rocahenry@gnail.com>
2021-02-08 15:08:01 +0000

    Structuring our app

--- cmd/server/mail.go
+++ cmd/server/mail.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type App struct {
+}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up our app")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go Rest API Course")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting rest api")
+	}
 }
